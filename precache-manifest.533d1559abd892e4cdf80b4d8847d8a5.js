6f5552415276614342cdd96b8928e0b315485b02
ahmadfreijeh <ahmadbfreijeh@gmail.com>
2019-09-17 15:24:38 +0300

    Updates

---
+++ precache-manifest.533d1559abd892e4cdf80b4d8847d8a5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d3546cd82efb1196307c480a79a823dc",
+    "url": "/currency-exchange-rates/index.html"
+  },
+  {
+    "revision": "2fd6eb75c2135854da6e",
+    "url": "/currency-exchange-rates/static/css/main.7502548c.chunk.css"
+  },
+  {
+    "revision": "92deb2171ab7205c0fe5",
+    "url": "/currency-exchange-rates/static/js/2.34d27849.chunk.js"
+  },
+  {
+    "revision": "2fd6eb75c2135854da6e",
+    "url": "/currency-exchange-rates/static/js/main.57e42ea9.chunk.js"
+  },
+  {
+    "revision": "f021a32e4c1945a58e2e",
+    "url": "/currency-exchange-rates/static/js/runtime~main.4e14d010.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/currency-exchange-rates/precache-manifest.00f1740195283227ddc2d359774e1c18.js"
+  "/currency-exchange-rates/precache-manifest.533d1559abd892e4cdf80b4d8847d8a5.js"
 );
 
 self.addEventListener('message', (event) => {
