53d397196ecd656011da297b15fe72cb1ff1bdcb
ahmadfreijeh <ahmadbfreijeh@gmail.com>
2019-09-17 13:37:01 +0300

    Updates

---
+++ precache-manifest.00f1740195283227ddc2d359774e1c18.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d0bbdfa8b23d57d485d05c177898a2d1",
+    "url": "/currency-exchange-rates/index.html"
+  },
+  {
+    "revision": "aa63a8b16d92fcdaa236",
+    "url": "/currency-exchange-rates/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "8970d5e3baef4caeccdf",
+    "url": "/currency-exchange-rates/static/js/2.b941de56.chunk.js"
+  },
+  {
+    "revision": "aa63a8b16d92fcdaa236",
+    "url": "/currency-exchange-rates/static/js/main.06fc9fae.chunk.js"
+  },
+  {
+    "revision": "f021a32e4c1945a58e2e",
+    "url": "/currency-exchange-rates/static/js/runtime~main.4e14d010.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/currency-exchange-rates/static/media/logo.5d5d9eef.svg"
+  }
+]);
